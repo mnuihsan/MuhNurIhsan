0cfa5cc02e063526941da4fab4bd956ffa5313a8
mnuihsan <134818403+mnuihsan@users.noreply.github.com>
2023-05-29 21:37:52 +0800

    Update Algoritma Genetik Ihsan.py

--- Algoritma Genetik Ihsan.py
+++ Algoritma Genetik Ihsan.py
@@ -15,29 +15,29 @@ jumlah_populasi = 6
 max_iterasi = 100
 
 def generate_chromosome():
-    chromosome = {
+    kromosom = {
         'a': random.randint(a_min, a_max),
         'b': random.randint(b_min, b_max),
         'c': random.randint(c_min, c_max),
         'd': random.randint(d_min, d_max)
     }
-    return chromosome
+    return kromosom
 
-def evaluate(chromosome):
-    equation_result = chromosome['a'] + 4 * chromosome['b'] + 2 * chromosome['c'] + 3 * chromosome['d']
-    fitness = abs(equation_result - 30)
+def evaluate(kromosom):
+    hasil_perhitungan = kromosom['a'] + 4 * kromosom['b'] + 2 * kromosom['c'] + 3 * kromosom['d']
+    fitness = abs(hasil_perhitungan - 30)
     return fitness
 
-def calculate_fitness(population):
-    total_fitness = sum(1 / (1 + evaluate(chromosome)) for chromosome in population)
-    return [1 / (1 + evaluate(chromosome)) / total_fitness for chromosome in population]
+def calculate_fitness(populasi):
+    total_fitness = sum(1 / (1 + evaluate(kromosom)) for kromosom in populasi)
+    return [1 / (1 + evaluate(kromosom)) / total_fitness for kromosom in populasi]
 
-def select_parent(population, fitness_values):
-    cumulative_probabilities = [sum(fitness_values[:i+1]) for i in range(len(fitness_values))]
-    random_value = random.random()
-    for i, probability in enumerate(cumulative_probabilities):
-        if random_value <= probability:
-            return population[i]
+def select_parent(populasi, fitness_values):
+    probabilitas_kumulatif = [sum(fitness_values[:i+1]) for i in range(len(fitness_values))]
+    nilai_acak = random.random()
+    for i, probability in enumerate(probabilitas_kumulatif):
+        if nilai_acak <= probability:
+            return populasi[i]
 
 def crossover(parent1, parent2):
     cut_point = random.randint(1, 3)  # Memilih posisi pemotongan acak
@@ -45,27 +45,27 @@ def crossover(parent1, parent2):
     child2 = {key: parent2[key] if index < cut_point else parent1[key] for index, key in enumerate(parent2)}
     return child1, child2
 
-def mutate(chromosome):
+def mutate(kromosom):
     mutated_gene = random.choice(['a', 'b', 'c', 'd'])
     if mutated_gene == 'a':
-        chromosome['a'] = random.randint(a_min, a_max)
+        kromosom['a'] = random.randint(a_min, a_max)
     elif mutated_gene == 'b':
-        chromosome['b'] = random.randint(b_min, b_max)
+        kromosom['b'] = random.randint(b_min, b_max)
     elif mutated_gene == 'c':
-        chromosome['c'] = random.randint(c_min, c_max)
+        kromosom['c'] = random.randint(c_min, c_max)
     elif mutated_gene == 'd':
-        chromosome['d'] = random.randint(d_min, d_max)
-    return chromosome
+        kromosom['d'] = random.randint(d_min, d_max)
+    return kromosom
 
 def genetic_algorithm():
     # Inisialisasi populasi awal
-    population = [generate_chromosome() for _ in range(jumlah_populasi)]
+    populasi = [generate_chromosome() for _ in range(jumlah_populasi)]
 
-    for iteration in range(max_iterasi):
-        fitness_values = calculate_fitness(population)
+    for iterasi in range(max_iterasi):
+        fitness_values = calculate_fitness(populasi)
 
         # Seleksi orang tua
-        parents = [select_parent(population, fitness_values) for _ in range(jumlah_populasi)]
+        parents = [select_parent(populasi, fitness_values) for _ in range(jumlah_populasi)]
 
         # Crossover
         offspring = []
@@ -82,21 +82,21 @@ def genetic_algorithm():
                 offspring[i] = mutate(offspring[i])
 
         # Menggabungkan populasi sebelumnya dengan offspring
-        population += offspring
+        populasi += offspring
 
         # Memilih populasi baru berdasarkan fitness
-        population = sorted(population, key=lambda x: evaluate(x))
-        population = population[:jumlah_populasi]
+        populasi = sorted(populasi, key=lambda x: evaluate(x))
+        populasi = populasi[:jumlah_populasi]
 
-        best_chromosome = population[0]
-        best_fitness = evaluate(best_chromosome)
+        kromosom_terbaik = populasi[0]
+        best_fitness = evaluate(kromosom_terbaik)
 
-        print(f"Iterasi {iteration+1}: Kromosom Terbaik: {best_chromosome}, Fitness: {best_fitness}")
+        print(f"Iterasi {iterasi+1}: Kromosom Terbaik: {kromosom_terbaik}, Fitness: {best_fitness}")
 
         if best_fitness == 0:
             break
 
-    return best_chromosome
+    return kromosom_terbaik
 
 # Menjalankan algoritma genetika
 nilai_mutasi = 0.1
